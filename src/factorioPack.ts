a436e181bbc73843b42d30abf80aba4cd3452ec5
David <Murnto@users.noreply.github.com>
2018-12-14 14:55:54 +1100

    Extract locale functions

--- src/factorioPack.ts
+++ src/factorioPack.ts
@@ -4,19 +4,7 @@ import { FactorioMod } from "./factorioMod";
 import { FactorioLuaEngine } from "./factorioLuaEngine";
 import * as ini from "ini";
 import merge = require("lodash.merge");
-import assert = require("assert");
-
-interface LocaleContext {
-    defaultSections?: string[];
-    lang: string;
-    locale: { [lang: string]: { [section: string]: { [key: string]: string } } };
-}
-
-interface LocalisationResult {
-    data: string;
-    partial?: boolean;
-    success: boolean;
-}
+import { resolveLocale } from "./factorioLocale";
 
 export class FactorioPack {
     public modLoadOrder: string[] = ["core"]; // core is always first
@@ -146,7 +134,7 @@ export class FactorioPack {
 
         for (const item of Object.values(data.item)) {
             this.applyDifficulty(item, "normal");
-            this.resolveLocale(item, ctx);
+            resolveLocale(item, ctx);
         }
 
         for (const rName of Object.keys(data.recipe)) {
@@ -157,7 +145,7 @@ export class FactorioPack {
             if (recipe.name === "angels-nitinol-smelting-1") {
                 console.log("a");
             }
-            this.resolveLocale(recipe, {
+            resolveLocale(recipe, {
                 ...ctx,
                 defaultSections: ["recipe-name", ...ctx.defaultSections],
             });
@@ -165,7 +153,7 @@ export class FactorioPack {
 
         for (const technology of Object.values(data.technology)) {
             this.applyDifficulty(technology, "normal");
-            this.resolveLocale(technology, {
+            resolveLocale(technology, {
                 ...ctx,
                 defaultSections: ["technology-name", ...ctx.defaultSections],
             });
@@ -226,27 +214,6 @@ export class FactorioPack {
         delete recipe.result;
     }
 
-    private internalResolveLocaleString(section: string | null, key: string, ctx: LocaleContext): LocalisationResult {
-        if (ctx.locale[ctx.lang] === undefined) {
-            return { data: `{${section}.${key}}`, success: false };
-        }
-        if (section === null) {
-            if (ctx.locale[ctx.lang][key] === undefined) {
-                return { data: `{${key}}`, success: false };
-            }
-
-            return { data: (ctx.locale[ctx.lang][key] as any) as string, success: true };
-        }
-        if (ctx.locale[ctx.lang][section] === undefined) {
-            return { data: `{${section}.${key}}`, success: false };
-        }
-        if (ctx.locale[ctx.lang][section][key] === undefined) {
-            return { data: `{${section}.${key}}`, success: false };
-        }
-
-        return { data: ctx.locale[ctx.lang][section][key], success: true };
-    }
-
     private loadData(): any {
         return new FactorioLuaEngine(this).load();
     }
@@ -279,117 +246,5 @@ export class FactorioPack {
         return locales;
     }
 
-    private resolveLocale(obj: any, ctx: LocaleContext) {
-        let loc: LocalisationResult;
-
-        if (obj.localised_name !== undefined) {
-            loc = this.resolveLocaleFmt(obj, ctx);
-
-            delete obj.localised_name;
-        } else {
-            loc = this.resolveLocaleString(obj.name, ctx);
-        }
-
-        obj.title = loc.data;
-
-        if (!loc.success) {
-            console.log("Didn't find locale for", obj);
-        }
-    }
-
-    private resolveLocaleFmt(obj: any, ctx: LocaleContext): LocalisationResult {
-        assert(Array.isArray(obj.localised_name));
-
-        const locFmt = this.resolveLocaleString(obj.localised_name[0], ctx);
-
-        if (!locFmt.success) {
-            return {
-                data: `${obj.localised_name[0]}-${obj.localised_name[1].join("-")}`,
-                success: false,
-            };
-        }
-
-        if (obj.localised_name[1] === undefined) {
-            return locFmt;
-        }
-
-        let locKeys: string[];
-
-        if (typeof obj.localised_name[1] === "string") {
-            locKeys = [obj.localised_name[1]];
-        } else if (Array.isArray(obj.localised_name[1])) {
-            locKeys = obj.localised_name[1];
-        } else {
-            console.log(":(");
-            throw Error("What is obj.localised_name[1]?");
-        }
-
-        for (let i = 0; i < locKeys.length; i++) {
-            const locParam = this.resolveLocaleString(locKeys[i], ctx);
-
-            locFmt.data = locFmt.data.split(`__${i + 1}__`).join(locParam.data);
-        }
-
-        return locFmt;
-    }
-
-    private resolveLocaleString(locKey: string, ctx: LocaleContext): LocalisationResult {
-        const spl = locKey.indexOf(".") !== -1 ? locKey.split(".", 2) : [null, locKey];
-        const [section, key] = spl;
-
-        const tierIndex = key!.lastIndexOf("-");
-        let tierKey: string | undefined;
-        let tierValue: number | undefined;
-
-        if (tierIndex !== -1) {
-            tierValue = parseInt(key!.slice(tierIndex + 1), 10);
-
-            if (!isNaN(tierValue)) {
-                tierKey = key!.slice(0, tierIndex);
-            }
-        }
-
-        const origLoc = this.internalResolveLocaleString(section, key!, ctx);
-        if (origLoc.success) {
-            return origLoc;
-        }
-
-        if (tierKey !== undefined) {
-            const origLocTier = this.internalResolveLocaleString(section, tierKey, ctx);
-
-            if (origLocTier.success) {
-                return {
-                    ...origLocTier,
-                    data: origLocTier.data + " " + tierValue,
-                };
-            }
-        }
-
-        if (ctx.defaultSections) {
-            for (const fallback of ctx.defaultSections) {
-                const loc = this.internalResolveLocaleString(fallback, key!, ctx);
-
-                if (loc.success) {
-                    return loc;
-                }
-
-                if (tierKey === undefined) {
-                    continue;
-                }
-
-                const locTier = this.internalResolveLocaleString(fallback, tierKey!, ctx);
-
-                if (!locTier.success) {
-                    continue;
-                }
-
-                return {
-                    ...locTier,
-                    data: locTier.data + " " + tierValue,
-                };
-            }
-        }
-
-        return origLoc;
     }
 }
